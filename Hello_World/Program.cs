3f0ae6c8d2a6fbcf9f213d9e1816e2edb6650360
Gayashan <hansagayashan7@gmail.com>
2025-05-10 12:56:13 +0530

    initialize dotnet, install package and generat password

---
+++ Hello_World/Program.cs
@@ -0,0 +1,11 @@
+﻿using PasswordGenerator;
+Console.WriteLine("Enter your name: ");
+var name= Console.ReadLine();
+var date= DateTime.Now;
+
+var pwd= new Password();
+var password=pwd.Next();
+
+Console.WriteLine($"{Environment.NewLine}Hello ,{name} , today is ,{date}, password is {password}.");
+
+Console.ReadKey(true);
